e87436117544925e5f3359bd53bc9e48f5915776
Ramahatsangiarison Hobiniaina Mirado <miradotsangy@gmail.com>
2020-05-23 15:44:12 +0300

    Remove frm cart

--- ecommerce/carts/urls.py
+++ ecommerce/carts/urls.py
@@ -4,6 +4,7 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
+    re_path('carts/(?P<id>\d+)/', views.remove_from_cart, name='remove_from_cart'),
     re_path('carts/(?P<slug>[\w-]+)/', views.add_to_cart, name='add_to_cart'),
     path('carts/', views.view, name='my_cart'),
 ]
--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -13,6 +13,15 @@ def view(request):
     if the_id:
         try:
             cart = Cart.objects.get(id=the_id)
+            new_total = 0.00
+            for item in cart.cartitem_set.all():
+                line_total = float(item.product.price) * float(item.quantity)
+                new_total += line_total
+
+            request.session['items_total'] = cart.cartitem_set.count()
+            cart.total = new_total
+            cart.save()
+
             context = {'cart': cart}
         except Cart.DoesNotExist:
             cart = None
@@ -56,21 +65,27 @@ def add_to_cart(request, slug):
                 product_var.append(v)
             except:
                 pass
-
         cart_item = CartItem.objects.create(cart=cart, product=product)
         if len(product_var) > 0:
             cart_item.variations.add(*product_var)
         cart_item.quantity = qty
         cart_item.save()
+        # success message
+        return HttpResponseRedirect(reverse('carts:my_cart'))
+    # error message
+    return HttpResponseRedirect(reverse('carts:my_cart'))
 
-        new_total = 0.00
-        for item in cart.cartitem_set.all():
-            line_total = float(item.product.price) * float(item.quantity)
-            new_total += line_total
 
-        request.session['items_total'] = cart.cartitem_set.count()
-        cart.total = new_total
-        cart.save()
+def remove_from_cart(request, id):
+    try:
+        the_id = request.session['cart_id']
+        cart = Cart.objects.get(id=the_id)
+    except:
         return HttpResponseRedirect(reverse('carts:my_cart'))
 
+    cartitem = CartItem.objects.get(id=id)
+    # cartitem.delete()
+    cartitem.cart = None
+    cartitem.save()
+    # Send success message
     return HttpResponseRedirect(reverse('carts:my_cart'))
