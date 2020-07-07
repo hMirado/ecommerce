4df9e5c7921ba35571fb41c51cc8db024a673058
ramahatsangiarisonhobiniainamirado <miradotsangy@gmail.com>
2020-07-07 13:23:15 +0300

    Django timezone (pip install pytz)

--- ecommerce/marketing/models.py
+++ ecommerce/marketing/models.py
@@ -1,4 +1,6 @@
+# import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class MarketingMessageQueryset (models.query.QuerySet):
@@ -6,7 +8,9 @@ class MarketingMessageQueryset (models.query.QuerySet):
         return self.filter(active=True)
 
     def featured(self):
-        return self.filter(featured=True)
+        return self.filter(featured=True)\
+            .filter(start_date__lt=timezone.now())\
+            .filter(end_date__gte=timezone.now())
 
 
 class MarketingMessageManager(models.Manager):
@@ -15,13 +19,13 @@ class MarketingMessageManager(models.Manager):
 
     def all(self):
         return self.get_queryset().active()
-    
+
     def featured(self):
         return self.get_queryset().filter(active=True).filter(featured=True)
 
     def get_featured_item(self):
         try:
-            return self.get_queryset().active().featured().order_by("-start_date")[0]
+            return self.get_queryset().active().featured()[0]
         except:
             return None
 
@@ -32,11 +36,18 @@ class MarketingMessage(models.Model):
     featured = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    start_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
-    end_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    start_date = models.DateTimeField(
+        auto_now=False, auto_now_add=False, null=True, blank=True)
+    end_date = models.DateTimeField(
+        auto_now=False, auto_now_add=False, null=True, blank=True)
 
     objects = MarketingMessageManager()
     # messages = MarketingMessageManager()
 
     def __str__(self):
         return str(self.message[:12])
+
+    class Meta:
+        ordering = ['-start_date', '-end_date']
+
+# pip instal pytz
